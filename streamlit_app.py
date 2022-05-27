5a28d4cbd57eb6df4b2f604e34e1f5aa294ef850
Machado Marques <ricardo.machadomarques@siemens.com>
2022-05-27 15:46:59 +0100

    added pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+#Let's put a pick list 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)

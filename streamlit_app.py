7eaffc175aab6aa4015f4092c58a8f34b0a5fdfe
Machado Marques <ricardo.machadomarques@siemens.com>
2022-06-01 16:59:07 +0100

    final commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,7 @@ except URLError as e:
 if streamlit.button("Get Fruit Load List"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_row = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_row)
 
 # add an entry to the list
@@ -61,4 +62,5 @@ add_my_fruit = streamlit.text_input("What fruit would you like to add")
 if streamlit.button("Add a Fruit to the list"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)

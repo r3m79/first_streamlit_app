34cc28b1fb2c2ecfd89b15a939ccac56a9011a6e
Machado Marques <ricardo.machadomarques@siemens.com>
2022-06-01 16:21:40 +0100

    new input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit list contains")
 streamlit.dataframe(my_data_row)
+
+#add an entry to the list
+add_my_fruit = streamlit.text_input("What fruit would you like to add")
+streamlit.write('Thanks for adding',add_my_fruit)
